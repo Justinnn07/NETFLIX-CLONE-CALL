909e4261c0941495e3a6e5d78c18578f3d8803f7
Justinnn07 <justinv2006@gmail.com>
2021-06-08 21:39:26 +0530

    added new component :fire:

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
 import { request } from "./Api/Api";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Row from "./components/Row";
+
+const image_base_url = "https://image.tmdb.org/t/p/original/";
 
 const App = () => {
+  const [popular, setPopular] = useState([]);
+
   useEffect(() => {
     axios
-      .get(`https://api.themoviedb.org/3${request.fetchActionMovies}`)
-      .then((res) => console.log(res));
-  }, []);
+      .get(`https://api.themoviedb.org/3${request.fetchTrending}`)
+      .then((res) => setPopular(res.data.results));
+  }, [setPopular]);
 
   return (
     <div className="app">
       <Header />
       <Home />
+      <h3>Popular on Netflix</h3>
+      <div className="app__row">
+        {popular.map((popular) => (
+          <div>
+            <Row img={`${image_base_url}${popular.poster_path}`} />
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
